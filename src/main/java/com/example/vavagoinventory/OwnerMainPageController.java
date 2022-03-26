d5a33f4a58a00af5896d7395d989b24c3d0820aa
xabsolon <xabsolon@stuba.sk>
2022-03-26 20:09:41 +0100

    Add side tab navigator

--- src/main/java/com/example/vavagoinventory/OwnerMainPageController.java
+++ src/main/java/com/example/vavagoinventory/OwnerMainPageController.java
@@ -1,20 +1,35 @@
 package com.example.vavagoinventory;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class OwnerMainPageController implements Initializable {
 
+    @FXML
+    private Button logOutButton;
+
+    public Log log = new Log();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        logOutButton.setOnAction(this::onLogOutButtonClick);
     }
 
-    @FXML
-    private void exitButtonClicked() {
-        System.exit(0);
+    private void onLogOutButtonClick(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(FadingIntroController.class.getResource("Login.fxml"));
+        try {
+            FunctionsController.changeScene(
+                    FunctionsController.getStageFromEvent(actionEvent), loader, "GoInventory");
+        } catch (IOException e) {
+            log.Exceptions("Failed to load login screen",e);
+        }
+        log.userLogout("ferino");
     }
 }
