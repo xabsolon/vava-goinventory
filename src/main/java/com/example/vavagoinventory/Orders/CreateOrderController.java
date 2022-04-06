1555b516e3ed47025e1482b985b8f24127939589
JozefKyska <79149113+JKyska@users.noreply.github.com>
2022-04-06 17:27:16 +0200

    fixed some stuff

--- src/main/java/com/example/vavagoinventory/Orders/CreateOrderController.java
+++ src/main/java/com/example/vavagoinventory/Orders/CreateOrderController.java
@@ -1,5 +1,6 @@
 package com.example.vavagoinventory.Orders;
 
+import com.example.vavagoinventory.DatabaseContextSingleton;
 import com.example.vavagoinventory.FunctionsController;
 import com.example.vavagoinventory.I18N;
 import javafx.fxml.FXML;
@@ -7,9 +8,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.jooq.DSLContext;
+import org.jooq.Record;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static org.jooq.codegen.maven.goinventory.Tables.ORDERS;
 
 public class CreateOrderController implements Initializable { //work in progress
 
