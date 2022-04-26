7093f5af692bf10713159dec1886e2ff71e695de
JozefKyska <79149113+JKyska@users.noreply.github.com>
2022-04-26 12:26:01 +0200

    all functions work now

--- src/main/java/com/example/vavagoinventory/UserManagement/CreateUserController.java
+++ src/main/java/com/example/vavagoinventory/UserManagement/CreateUserController.java
@@ -1,15 +1,32 @@
 package com.example.vavagoinventory.UserManagement;
 
+import com.example.vavagoinventory.DatabaseContextSingleton;
+import com.example.vavagoinventory.FunctionsController;
+import com.example.vavagoinventory.I18N;
+import com.example.vavagoinventory.Orders.Order;
 import com.example.vavagoinventory.I18N;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.exception.DataAccessException;
 import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static org.jooq.codegen.maven.goinventory.Tables.ORDERS;
+import static org.jooq.codegen.maven.goinventory.Tables.PRODUCTS;
+import static org.jooq.impl.DSL.inline;
+import static org.jooq.impl.DSL.select;
+
 public class CreateUserController implements Initializable {
+
+    private UserManagementController userManagementController;
+
     @FXML
     private Button confirmCreateButton;
 
@@ -28,6 +45,15 @@ public class CreateUserController implements Initializable {
     @FXML
     private Label createUserLabel;
 
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField positionField;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         confirmCreateButton.textProperty().bind(I18N.createStringBinding("confirmButtonLabel"));
@@ -36,5 +62,43 @@ public class CreateUserController implements Initializable {
         passwordLabel.textProperty().bind(I18N.createStringBinding("passwordLabel"));
         positionLabel.textProperty().bind(I18N.createStringBinding("positionLabel"));
         createUserLabel.textProperty().bind(I18N.createStringBinding("createUserLabel"));
+
+    }
+
+    public void injectUserManagementController(UserManagementController userManagementController) {
+        this.userManagementController = userManagementController;
+    }
+
+    @FXML
+    private void onClickCreate() {
+        User user;
+        if (!positionField.getText().equals("owner")
+                && !positionField.getText().equals("user")
+                && !positionField.getText().equals("logistics")) {
+            FunctionsController.showErrorAlert("Invalid position");
+            return;
+        }
+        user = new User.UserBuilder()
+                .username(usernameField.getText())
+                .password(passwordField.getText())
+                .position(positionField.getText())
+                .build();
+        try {
+            UserManagementController.UserQuery.insertQuery(user);
+        }
+        catch (DataAccessException e) {
+            FunctionsController.showErrorAlert("Cannot create order, email must be unique");
+            return;
+        }
+        userManagementController.addUser(user);
+
+        Stage stage = (Stage) cancelCreateButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void onClickCancel() {
+        Stage stage = (Stage) cancelCreateButton.getScene().getWindow();
+        stage.close();
     }
 }
--- src/main/java/com/example/vavagoinventory/UserManagement/EditUserController.java
+++ src/main/java/com/example/vavagoinventory/UserManagement/EditUserController.java
@@ -1,16 +1,34 @@
 package com.example.vavagoinventory.UserManagement;
 
 import com.example.vavagoinventory.I18N;
+import com.example.vavagoinventory.FunctionsController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.jooq.exception.DataAccessException;
 
 import java.net.URL;
 import java.nio.Buffer;
 import java.util.ResourceBundle;
 
 public class EditUserController implements Initializable {
+
+    private UserManagementController userManagementController;
+
+    private User selected;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField positionField;
+
     @FXML
     private Button confirmCreateButton;
 
@@ -37,5 +55,55 @@ public class EditUserController implements Initializable {
         passwordLabel.textProperty().bind(I18N.createStringBinding("passwordLabel"));
         positionLabel.textProperty().bind(I18N.createStringBinding("positionLabel"));
         editUserLabel.textProperty().bind(I18N.createStringBinding("editUserLabel"));
+
+    }
+
+    public void injectUserManagementController(UserManagementController userManagementController) {
+        this.userManagementController = userManagementController;
+        selected = userManagementController.getLastSelected();
+        usernameField.setText(selected.getUsername());
+        passwordField.setText(selected.getPassword());
+        positionField.setText(selected.getPosition());
+    }
+
+
+    @FXML
+    private void onClickCreate() {
+        User user;
+        if (!positionField.getText().equals("owner")
+                && !positionField.getText().equals("user")
+                && !positionField.getText().equals("logistics")) {
+            FunctionsController.showErrorAlert("Invalid position");
+            return;
+        }
+
+        user = new User.UserBuilder()
+                .username(usernameField.getText())
+                .password(passwordField.getText())
+                .position(positionField.getText())
+                .id(selected.getId())
+                .build();
+
+        try {
+            UserManagementController.UserQuery.editQuery(user);
+        }
+        catch (DataAccessException e) {
+            FunctionsController.showErrorAlert("Cannot edit order, email must be unique");
+            return;
+        }
+
+        selected.setUsername(user.getUsername());
+        selected.setPassword(user.getPassword());
+        selected.setPosition(user.getPosition());
+        userManagementController.updateTable();
+
+        Stage stage = (Stage) cancelCreateButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void onClickCancel() {
+        Stage stage = (Stage) cancelCreateButton.getScene().getWindow();
+        stage.close();
     }
 }
